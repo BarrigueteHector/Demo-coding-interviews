e35c2bc968779ba8386266bf4d74db37d744233b
Barriguete Hector <50279990+BarrigueteHector@users.noreply.github.com>
2025-05-01 20:25:45 -0600

    Update and rename ejercicio2.py to Warm-up Challenges/ejercicio2.py

--- Warm-up Challenges/ejercicio2.py
+++ Warm-up Challenges/ejercicio2.py
@@ -4,13 +4,6 @@ import random
 import re
 import sys
 
-#
-# Complete the 'jumpingOnClouds' function below.
-#
-# The function is expected to return an INTEGER.
-# The function accepts INTEGER_ARRAY c as parameter.
-#
-
 def jumpingOnClouds(c):
     len_c = len(c)
     count_steps = 0
@@ -40,16 +33,3 @@ def jumpingOnClouds(c):
     return count_steps - 1
 
 jumpingOnClouds([0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0]);
-
-# if __name__ == '__main__':
-#     fptr = open(os.environ['OUTPUT_PATH'], 'w')
-
-#     n = int(input().strip())
-
-#     c = list(map(int, input().rstrip().split()))
-
-#     result = jumpingOnClouds(c)
-
-#     fptr.write(str(result) + '\n')
-
-#     fptr.close()
